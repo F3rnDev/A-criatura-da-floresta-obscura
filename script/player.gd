84c76e42d870a9998d49bc8efe324bfaf8cb8b51
Otávio Luiz <otaviopax@gmail.com>
2023-10-02 19:33:22 -0300

    Colisões com paredes
    - Criado template de parede
    - Colisão com parede feita

--- script/player.gd
+++ script/player.gd
@@ -1,40 +1,25 @@
-extends Area2D
-
-@export var speed = 400
-var screen_size
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
+extends CharacterBody2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x += -1
-	if Input.is_action_pressed("move_up"):
-		velocity.y += -1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+const SPEED = 300.0
+
+
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var dirX = Input.get_axis("move_left", "move_right")
+	var dirY = Input.get_axis("move_down", "move_up")
 	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+	if dirX:
+		velocity.x = dirX * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if dirY:
+		velocity.y = dirY * -SPEED
 	else: 
-		$AnimatedSprite2D.stop()
-		
-	
-	if velocity.y < 0:
-		$AnimatedSprite2D.animation = "up" 
-	elif velocity.y > 0: 
-		$AnimatedSprite2D.animation = "down"	
-	elif velocity.x < 0: 
-		$AnimatedSprite2D.animation = "left"	
-	elif velocity.x > 0: 
-		$AnimatedSprite2D.animation = "right"
-	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-		
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
